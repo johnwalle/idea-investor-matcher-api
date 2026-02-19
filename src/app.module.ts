b044b762145bd767c6a4acfdb3655e114e4c2501
mark133227 <etawe1627@gmail.com>
2026-02-19 10:04:27 +0300

    implement complete investor onbaording moudle with prisma model, enums, validation, secured endpoint, and role based access control

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './database/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { InvestorModule } from './investor/investor.moudle';
 
 @Module({
-  imports: [PrismaModule, UsersModule, AuthModule],
+  imports: [PrismaModule, AuthModule, UsersModule, InvestorModule],
 })
 export class AppModule {}
