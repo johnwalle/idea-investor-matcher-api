f0ea8000c7d1cd0dbccc71b07d72820960800ffd
mark133227 <etawe1627@gmail.com>
2026-02-21 16:58:38 +0300

    implement idea creation with optional pitch upload, cloudinary integration, validation and prisma schema updates

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { UsersModule } from './users/users.module';
 import { PrismaModule } from './database/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { InvestorModule } from './investor/investor.moudle';
+import { EntrepreneurModule } from './entrepreneur/entrepreneur.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule, InvestorModule],
+  imports: [PrismaModule, AuthModule, UsersModule, InvestorModule, EntrepreneurModule],
 })
 export class AppModule {}
