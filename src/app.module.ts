b24100081d7a5da6869d0b38351029b5eb2e718f
mark133227 <etawe1627@gmail.com>
2026-03-10 21:32:10 +0300

    add global Redis module with ioredis client

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { PrismaModule } from './database/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { InvestorModule } from './investor/investor.moudle';
 import { EntrepreneurModule } from './entrepreneur/entrepreneur.module';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule, InvestorModule, EntrepreneurModule],
+  imports: [PrismaModule, AuthModule, UsersModule, InvestorModule, EntrepreneurModule, RedisModule],
 })
 export class AppModule {}
