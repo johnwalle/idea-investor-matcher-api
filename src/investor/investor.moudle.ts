768b875061c7474c90134fd140949343368b25d8
mark133227 <etawe1627@gmail.com>
2026-03-13 12:51:17 +0300

    import chatModule into investorModule to resolve chatService dependency

--- src/investor/investor.moudle.ts
+++ src/investor/investor.moudle.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { InvestorService } from './investor.service';
 import { InvestorController } from './investor.controller';
 import { PrismaModule } from '../database/prisma.module';
+import { ChatModule } from 'src/chat/chat.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ChatModule],
   providers: [InvestorService],
   controllers: [InvestorController],
 })
-export class InvestorModule {}
+export class InvestorModule { }
