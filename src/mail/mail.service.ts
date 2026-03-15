f309c200d67c1d6a203657b40a1cc72355ee3462
mark133227 <etawe1627@gmail.com>
2026-03-15 13:15:41 +0300

    mail service updated

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -7,7 +7,9 @@ export class MailService {
 
   constructor() {
     this.transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true, // use SSL
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
