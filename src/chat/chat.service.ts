8e1eeebaabdbb019441f56299e818fb09ffdf5d9
Yared3214 <yaredman0099@gmail.com>
2026-03-20 17:46:55 +0300

    added slight change 3

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -128,7 +128,7 @@ export class ChatService {
         messages: {
           orderBy: { createdAt: 'desc' },
           take: 1, // last message preview
-          select: { read: true, content: true, createdAt: true, roomId: true },
+          select: { read: true, content: true, createdAt: true, roomId: true, senderId: true },
         },
       },
       orderBy: { createdAt: 'desc' },
@@ -149,6 +149,3 @@ export class ChatService {
     });
   }
 }
-
-
-
